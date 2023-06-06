3aa0eef7cd00497305f04ad39a318c92293fa7d5
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 19:10:08 +0200

    Fix function

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,13 @@
 export const convertPLNToUSD = (PLN) => {
-
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
   const PLNtoUSD = PLN / 3.5;
-  
+  debugger;
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
-
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+};

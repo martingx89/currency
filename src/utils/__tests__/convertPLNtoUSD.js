1248ceae43bf5517b0de5856988a95e7f4b0b3d3
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 17:53:54 +0200

    Add first test

---
+++ src/utils/__tests__/convertPLNtoUSD.js
@@ -0,0 +1,10 @@
+import { convertPLNToUSD } from './../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+});

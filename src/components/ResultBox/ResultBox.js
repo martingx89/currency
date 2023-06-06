6511479ab45e98149230e0b90403c7e688eb513e
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 20:44:05 +0200

    Test output

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -6,17 +6,16 @@ import { useMemo } from 'react';
 import styles from './ResultBox.module.scss';
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
+    if (from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
   const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
 
   return (
-    <div className={styles.result}>
+    <div data-testid='output' className={styles.result}>
       {formattedAmount} = {convertedAmount}
     </div>
   );
@@ -26,6 +25,6 @@ ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-}
+};
 
 export default ResultBox;
--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,8 +1,15 @@
 import ResultBox from './ResultBox';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Component ResultBox', () => {
   it('should render without crashing', () => {
     render(<ResultBox from='PLN' to='USD' amount={100} />);
+  });
+
+  it('should render proper info about conversion when PLN -> USD', () => {
+    render(<ResultBox from='PLN' to='USD' amount={100} />);
+    const output = screen.getByTestId('output');
+    expect(output).toHaveTextContent('PLN 100.00 = $28.57');
   });
 });

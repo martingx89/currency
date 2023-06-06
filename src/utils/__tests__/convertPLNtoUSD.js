00d8d4c20714ee8be7bf085ff077db7aa80ce9b0
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 18:02:56 +0200

    Test with NaN

--- src/utils/__tests__/convertPLNtoUSD.js
+++ src/utils/__tests__/convertPLNtoUSD.js
@@ -7,4 +7,9 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
   });
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-543')).toBeNaN();
+  });
 });

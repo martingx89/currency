5061f808f6afe9afffe49988f1d32ee94719b1dd
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 19:40:23 +0200

    Extend tests

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
 
 describe('Component CurrencyForm', () => {
   it('should render without crashing', () => {
@@ -10,5 +11,14 @@ describe('Component CurrencyForm', () => {
 
     // render component
     render(<CurrencyForm action={action} />);
+
+    // find “convert” button
+    const submitButton = screen.getByText('Convert');
+
+    // simulate user click on "convert" button
+    userEvent.click(submitButton);
+
+    // check if action callback was called once
+    expect(action).toHaveBeenCalledTimes(1);
   });
 });

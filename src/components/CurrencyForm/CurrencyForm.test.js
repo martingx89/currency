f445392a2c8d4d9c24f38157639e767c974a69dd
Marcin Adamiak <marcin_89@outlook.com>
2023-06-06 19:34:04 +0200

    Add interaction test

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -5,4 +5,10 @@ describe('Component CurrencyForm', () => {
   it('should render without crashing', () => {
     render(<CurrencyForm action={() => {}} />);
   });
+  it('should run action callback with proper data on form submit', () => {
+    const action = jest.fn();
+
+    // render component
+    render(<CurrencyForm action={action} />);
+  });
 });
